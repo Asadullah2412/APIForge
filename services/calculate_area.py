24c9094e01cd9ff10eeb9901c967aea1e37f72c9
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-09-20 20:00:25 +0300

    minor change

--- services/calculate_area.py
+++ services/calculate_area.py
@@ -1,10 +1,17 @@
 # calculates the area of a rectangle
+from fastapi import status
 
 def area_rectangle(length:float,width:float):
-    area = length * width
+    if length and width > 0:
+        area = length * width
 
-    return {
+        return {
         "area" : area
-    }
+        }
+    else: 
+        return status.HTTP_400_BAD_REQUEST
     
 
+
+x = area_rectangle(length=20,width=100)
+print(x)
