8a428a9c2971582793af3b7986000878c983a3f5
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-09-20 20:04:59 +0300

    minor change 2

--- services/isPlalindrome.py
+++ services/isPlalindrome.py
@@ -1,10 +1,13 @@
 # returns true if it is plalindorm or return false
-
+from fastapi import status
 def isPlaindrome(word:str):
+    if word.isspace() == False:
 
-    return {
-        "isPlaindrome" : (word == word[::-1])
-    }
+        return {
+            "isPlaindrome" : (word == word[::-1])
+        }
+    else:
+        return status.HTTP_400_BAD_REQUEST
 
-x = isPlaindrome(word="racecar")
+x = isPlaindrome(word="  ")
 print(x)
